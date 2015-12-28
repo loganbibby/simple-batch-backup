d58d386befe3c22fe73a8a172a441ce47fc47d19
Logan Bibby <sp0rk.me.with.a.knife@gmail.com>
2015-12-28 11:13:26 -0600

    Update Backup.bat
    Updated switches. Added example additional directory.

--- Backup.bat
+++ Backup.bat
@@ -2,7 +2,7 @@
 setlocal EnableDelayedExpansion
 :: variables
 set destination=C:\Backup
-set xcopyswitches=/c /i /e /y
+set xcopyswitches=/s /e /h /i /y
 
 :: welcome message
 echo File backup.
@@ -15,7 +15,7 @@ set /a n=0
 set user_directories=Documents Downloads Pictures Music Videos
 for %%a in (%user_directories%) do (
 	set dest_directories[!n!]=%%a
-	set sour_directories[!n!]=%USERPROFILE%\%%a
+	set sour_directories[!n!]="%USERPROFILE%\%%a"
 	set /a n = n + 1
 )
 
@@ -23,6 +23,10 @@ for %%a in (%user_directories%) do (
 ::  1. add the destination folder to dest_directories array (use %n% as the counter)
 ::  2. add the source folder to the sour_directories array (again, use %n%)
 ::  3. increase the n variable by 1
+
+::set dest_directories[!n!]=PowerShell
+::set sour_directories[!n!]="C:\Program Files (x86)\WindowsPowerShell"
+::set /a n = n + 1
 
 :: remove the last element
 set /a n = n -1
@@ -33,7 +37,7 @@ echo/
 echo/
 for /L %%i in (0,1,%n%) do (
 	echo Directory: !sour_directories[%%i]!
-	xcopy "!sour_directories[%%i]!" "%destination%\!dest_directories[%%i]!" %xcopyswitches%
+	xcopy !sour_directories[%%i]! "%destination%\!dest_directories[%%i]!" %xcopyswitches%
 	echo/
 )
 echo/
